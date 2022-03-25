51b83ddf488287ea75b9b2717b7e77aff13d01ae
Parth <parthnanda@Parths-MacBook-Air.local>
2022-03-25 15:17:06 +0530

    small changes

--- app/src/main/java/com/example/firesafety/help.java
+++ app/src/main/java/com/example/firesafety/help.java
@@ -2,7 +2,10 @@ package com.example.firesafety;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class help extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class help extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
+    }
+
+    public void OpenTabbed(View view) {
+        Intent callIntent = new Intent(Intent.ACTION_CALL);
+        callIntent.setData(Uri.parse("tel:101"));
+        startActivity(callIntent);
     }
 }
